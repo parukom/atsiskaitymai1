75586c3e74da75ebbe106ee993d16580427c6392
Tomas Dudovicius <tomasdudovicius@gmail.com>
2022-03-22 11:32:28 +0200

    devinta uzduotis Done

--- 50 paskaita 03.22 testas/Task 9/script.js
+++ 50 paskaita 03.22 testas/Task 9/script.js
@@ -6,3 +6,20 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+class Movie {
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+    }
+    wasExpensive() {
+        if (this.budget > 100000000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+const filmas = new Movie('Titanic', 'James Cameron', 200000000)
+console.log(filmas.wasExpensive());
