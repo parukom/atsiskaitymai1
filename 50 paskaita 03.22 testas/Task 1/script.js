76585155e2ff2c65484e61ad6744992778756950
Tomas Dudovicius <tomasdudovicius@gmail.com>
2022-03-22 08:52:13 +0200

    Pirma uzduotis Done

--- 50 paskaita 03.22 testas/Task 1/script.js
+++ 50 paskaita 03.22 testas/Task 1/script.js
@@ -8,3 +8,17 @@ pamatyti jo pateikto svorio konvertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+document.querySelector(`form`).addEventListener(`submit`, skaiciuokle);
+
+function skaiciuokle(e) {
+    e.preventDefault();
+    let ivestis = document.querySelector(`#search`).value;
+    document.querySelector(`#output`).innerHTML += `
+    Svoris kg: ${ivestis} <br>
+    Svoris lb: ${ivestis * 2.2046} <br>
+    Svoris g:  ${ivestis / 0.001} <br>
+    Svoris oz: ${ivestis * 35.274}
+    `;
+    ivestis.value = null;
+}
