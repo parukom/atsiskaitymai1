50cc8572c5d193593989f5fa77b956c62d70e1af
Tomas Dudovicius <tomasdudovicius@gmail.com>
2022-03-22 11:18:59 +0200

    astunta uzduotis Done

--- 50 paskaita 03.22 testas/Task 8/script.js
+++ 50 paskaita 03.22 testas/Task 8/script.js
@@ -5,3 +5,27 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą;
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+class Calculator {
+    sum = (a, b) => {
+        let answer1 = a + b;
+        return answer1;
+    };
+    subtraction = (a, b) => {
+        let answer2 = a - b;
+        return answer2;
+    };
+    multiplication = (a, b) => {
+        let answer3 = a * b;
+        return answer3;
+    };
+    division = (a, b) => {
+        let answer4 = a / b;
+        return answer4;
+    };
+}
+let skaiciuokle = new Calculator();
+console.log(`sudeties veiksmas 20 + 13 = ${skaiciuokle.sum(20, 13)}`);
+console.log(`atimties veiksmas 20 - 13 = ${skaiciuokle.subtraction(20, 13)}`);
+console.log(`daugybos veiksmas 20 * 13 = ${skaiciuokle.multiplication(20, 13)}`);
+console.log(`dalybos veiksmas 20 / 13 = ${skaiciuokle.division(20, 13)}`);
